2858fd987bffb0d5b7f73eaaa53a668760da7208
stephsingler <singlerstephanie@gmail.com>
2025-05-22 14:08:04 -0500

    background photo

--- singler-plc/src/components/BackgroundPhoto.js
+++ singler-plc/src/components/BackgroundPhoto.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import golden from '../images/goldengate.jpg'
+import vineyard from '../images/vineyard.jpg';
+import austin from '../images/austin.jpg';
 
 const BackgroundPhoto = () => {
   return (
     <div className="background-photo">
-      <img src="https://i.imgur.com/yswpEo6.jpg" alt="" />
+      <img src={golden} alt="" />
+      <img src={vineyard} alt="" />
+      <img src={austin} alt="" />
     </div>
   );
 };
--- singler-plc/src/components/ContactUs.js
+++ singler-plc/src/components/ContactUs.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ContactUs = () => {
   return (
     <div className="contact-us">
-      <button className="contact-button"> <a href="#contact">CONTACT US</a></button>
+      <button className="contact-button"> <a href="mailto:llf@singler-law.com">CONTACT US</a></button>
     </div>
   );
 };
