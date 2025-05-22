9c92a1612ac29d8b380df4d5c1ae962b0e1be0b7
stephsingler <singlerstephanie@gmail.com>
2025-05-21 19:47:13 -0500

    mail to

--- singler-plc/src/components/Footer.js
+++ singler-plc/src/components/Footer.js
@@ -1,13 +1,13 @@
 import React from 'react';
 //components
-import {FaFax, FaPhone} from "react-icons/lib/fa";
+import {FaEnvelope, FaPhone} from "react-icons/lib/fa";
 
 const Footer = () => {
   return (
     <div className="footer">
         <div>
-            <p><FaPhone size={15} style={{color: '#ffffff'}}/>&nbsp; (707) 823-8719</p>
-            <p><FaFax size={15} style={{color: '#ffffff'}} />&nbsp; (707) 823-8737</p>
+            <p><FaPhone size={20} style={{color: '#ffffff'}}/>&nbsp; (707) 823-8719</p>
+            <a href="mailto:info@singler-law.com"><FaEnvelope size={20} style={{color: '#ffffff'}} />&nbsp; info@singler-law.com</a>
         </div>
       <p className="copy">Singler Professional Law Corp. &copy; 2025</p>
     </div>
