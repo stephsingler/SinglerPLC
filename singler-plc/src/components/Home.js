e6e9cf2eec75387f68cc58cb8a3aaab7ccd8232e
stephsingler <singlerstephanie@gmail.com>
2025-05-22 14:22:12 -0500

    home testimonial

--- singler-plc/src/components/Home.js
+++ singler-plc/src/components/Home.js
@@ -3,12 +3,14 @@ import React from 'react';
 import BackgroundPhoto from './BackgroundPhoto';
 import Header from './Header';
 import ContactUs from './ContactUs';
+import Testimonial from "./Testimonial";
 
 const Home = () => {
   return (
     <div id='home'>
       <Header />
       <BackgroundPhoto />
+      <Testimonial />
       <ContactUs />
     </div>
   );
